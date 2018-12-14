b67c8a643ec55ef509207ffbe32916aa41627510
kobe08bryant24 <czf4048591590@163.com>
2018-12-14 19:57:58 +0800

    Initial commit

---
+++ src/main/java/com/qf/admin/pojo/po/Person.java
@@ -0,0 +1,33 @@
+package com.qf.admin.pojo.po;
+
+public class Person {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
